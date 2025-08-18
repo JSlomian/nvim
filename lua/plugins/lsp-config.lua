df8407284f05961bf598d1a487e4ce11a35be4f3
Jan Słomian <jslomian@multiprojekt.pl>
2025-08-18 10:35:42 +0200

    gitignore lazy

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -56,6 +56,7 @@ return {
           format = true,
         },
       })
+      --lspconfig.php
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
